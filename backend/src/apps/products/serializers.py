c472f19e85ce243ed93b17ad8d7e71e2687538b0
teenxsky <kutorgin2002@gmail.com>
2025-06-28 23:58:30 +1000

    Add factory-boy and faker, update ProductSerializer, remove tests.py

--- backend/src/apps/products/serializers.py
+++ backend/src/apps/products/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from apps.products.models import Product
 
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
